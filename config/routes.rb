05eca72f2e38759b0be6f5ab2606b245e71e4ab3
Justin Jungst <jjungstprogramming@gmail.com>
2017-05-09 10:23:49 -0500

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+  get '/about', to: 'pages#about'
+  get '/help', to: 'pages#help'
+
+  # get '/todos', to: 'todos#index'
+  # get '/todos/new', to: 'todos#new'
+  # post '/todos', to: 'todos#create'
+  # get '/todos/:id', to: 'todos#show'
+  # get '/todos/:id/edit', to: 'todos#edit'
+  # patch '/todos/:id', to: 'todos#update'
+  # delete '/todos/:id', to: 'todos#destroy'
+
+  resources :todos
+end
